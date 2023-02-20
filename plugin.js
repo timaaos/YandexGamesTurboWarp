321f5dede0f71e0fe07539c9840bc06034724ae3
Timaaos <75538611+timaaos@users.noreply.github.com>
2023-02-20 23:58:50 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -17,10 +17,7 @@ class YaGamesExtension {
     };
   }
   initsdk() {
-    // You can just return a value: any string, boolean, or number will work.
-    // If you have to perform an asynchronous action like a request, just return a Promise.
-    // The block will wait until the Promise resolves and return the resolved value.
-    return 'Hello, world!';
+    console.log("Trying to init SDK");
   }
 }
 
